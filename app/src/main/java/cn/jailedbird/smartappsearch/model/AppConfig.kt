c64b22d8848a32105e7b67409b43fd3cd76f8961
JailedBird <zjc986812982@gmail.com>
2022-12-20 16:17:16 +0800

    feat: code optimize

--- app/src/main/java/cn/jailedbird/smartappsearch/model/AppConfig.kt
+++ app/src/main/java/cn/jailedbird/smartappsearch/model/AppConfig.kt
@@ -1,12 +1,5 @@
 package cn.jailedbird.smartappsearch.model
 
-data class AppConfig(
-    /** Auto pop ime when App start*/
-    val popImeWhenStart: Boolean = true,
-    /** Right now launch app when only one search result*/
-    val launchRightNow: Boolean = false,
-) {
-    companion object {
-        const val LAUNCH_DELAY_TIME = 800L
-    }
+object AppConfig {
+    const val LAUNCH_DELAY_TIME = 800L
 }
