b65f7eef75fa7af48af82e8f681917cb16e65c14
JailedBird <zjc986812982@gmail.com>
2023-11-21 11:14:48 +0800

    feat: refactor expose gradle kts

--- gradle/expose/expose.gradle.kts
+++ gradle/expose/expose.gradle.kts
@@ -1,3 +1,13 @@
+@file:Suppress("PrivatePropertyName")
+
+import java.nio.file.FileSystems
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.StandardCopyOption
+import java.nio.file.attribute.BasicFileAttributes
 import kotlin.io.path.absolutePathString
 
 /* [Tutorial documentation]
@@ -18,10 +28,12 @@ extra["includeWithJavaApi"] = { module: String ->
 }
 
 private val DEFAULT_EXPOSE_DIR_NAME = "expose"
-private val SCRIPE_DIR = "$rootDir/gradle/expose/"
-private val BUILD_TEMPLATE_PATH_JAVA = "${SCRIPE_DIR}build_gradle_template_java"
-private val BUILD_TEMPLATE_PATH_ANDROID = "${SCRIPE_DIR}build_gradle_template_android"
+private val SCRIPT_DIR = "$rootDir/gradle/expose/"
+private val BUILD_TEMPLATE_PATH_JAVA = "${SCRIPT_DIR}build_gradle_template_java"
+private val BUILD_TEMPLATE_PATH_ANDROID = "${SCRIPT_DIR}build_gradle_template_android"
 private val ENABLE_FILE_CONDITION = false
+private val MODULE_NAMESPACE_TEMPLATE = "cn.jailedbird.module.%s_expose"
+
 
 private val DEFAULT_CONDITION: (String) -> Boolean = if (ENABLE_FILE_CONDITION) {
     ::ownCondition
@@ -39,17 +51,10 @@ fun includeWithApi(
     val moduleProject = project(module)
     val src = moduleProject.projectDir.absolutePath
     val des = "${src}_api"
-    // Ensure des directoy is created, avoid build.gradle create failed!
-    java.io.File(des).let {
-        if (!it.exists()) {
-            it.mkdir()
-        }
-    }
+    // generate build.gradle.kts
+    generateBuildGradle(des, "build.gradle.kts", moduleProject.name, isJava)
     doSync(src, expose, condition)
-    createBuildGradle(
-        "${des}${java.io.File.separator}build.gradle.kts",
-        moduleProject.name, isJava
-    )
+
     include("${module}_api")
 }
 
@@ -61,8 +66,8 @@ fun doSync(src0: String, expose: String, condition: (String) -> Boolean) {
     val root = java.io.File(src)
     val pathList = mutableListOf<String>()
     if (root.exists() && root.isDirectory) {
-        measure("findDirecoryByNio") {
-            findDirecoryByNIO(src, expose, pathList)
+        measure("findDirectoryByNio") {
+            findDirectoryByNIO(src, expose, pathList)
         }
     }
 
@@ -89,45 +94,45 @@ fun measure(tag: String, block: () -> Unit) {
     println("Measure: $tag spend ${System.currentTimeMillis() - t1}ms")
 }
 
-@Deprecated(message = "Deprecated because of low performance")
-fun findDirectoryByJavaIO(
-    root: java.io.File,
-    specPath: String,
-    pathList: MutableList<String>
-): String? {
-    if (!root.exists() || !root.isDirectory) {
-        return null
-    }
-    if (root.name == specPath) {
-        return root.absolutePath
-    }
-
-    for (file in root.listFiles()) {
-        if (file.isDirectory) {
-            val find = findDirectoryByJavaIO(file, specPath, pathList)
-            if (find != null) {
-                pathList.add(find)
-            }
-        }
-    }
-    return null
-}
+// @Deprecated(message = "Deprecated because of low performance")
+// fun findDirectoryByJavaIO(
+//     root: File,
+//     specPath: String,
+//     pathList: MutableList<String>
+// ): String? {
+//     if (!root.exists() || !root.isDirectory) {
+//         return null
+//     }
+//     if (root.name == specPath) {
+//         return root.absolutePath
+//     }
+//
+//     for (file in root.listFiles()) {
+//         if (file.isDirectory) {
+//             val find = findDirectoryByJavaIO(file, specPath, pathList)
+//             if (find != null) {
+//                 pathList.add(find)
+//             }
+//         }
+//     }
+//     return null
+// }
 
 /**
- * Better than [findDirectoryByJavaIO] with lowest time spend
+ * Better than findDirectoryByJavaIO with lowest time spend
  * */
-fun findDirecoryByNIO(dir: String, specPath: String, pathList: MutableList<String>) {
-    java.nio.file.Files.walkFileTree(java.nio.file.Paths.get(dir), object :
-        java.nio.file.SimpleFileVisitor<java.nio.file.Path>() {
+fun findDirectoryByNIO(dir: String, specPath: String, pathList: MutableList<String>) {
+    Files.walkFileTree(Paths.get(dir), object :
+        SimpleFileVisitor<Path>() {
         override fun preVisitDirectory(
-            dir: java.nio.file.Path,
-            attrs: java.nio.file.attribute.BasicFileAttributes?
-        ): java.nio.file.FileVisitResult {
+            dir: Path,
+            attrs: BasicFileAttributes?
+        ): FileVisitResult {
             return if (dir.fileName.toString() == specPath) {
                 pathList.add(dir.toAbsolutePath().toString())
-                java.nio.file.FileVisitResult.SKIP_SUBTREE
+                FileVisitResult.SKIP_SUBTREE
             } else {
-                java.nio.file.FileVisitResult.CONTINUE
+                FileVisitResult.CONTINUE
             }
         }
     })
@@ -138,31 +143,31 @@ fun findDirecoryByNIO(dir: String, specPath: String, pathList: MutableList<Strin
  * */
 fun deleteDirectoryByNio(dir: String) {
     try {
-        val path = java.nio.file.FileSystems.getDefault().getPath(dir)
-        if (!java.nio.file.Files.exists(path)) { // empty dir to check
+        val path = FileSystems.getDefault().getPath(dir)
+        if (!Files.exists(path)) { // empty dir to check
             println("empty path ${path.toAbsolutePath()} to delete")
             return
         }
         println("to delete path " + path.absolutePathString().toString())
-        // java.nio.file.Files.delete(path); // can not delete not empty dir
-        java.nio.file.Files.walkFileTree(
+        // Files.delete(path); // can not delete not empty dir
+        Files.walkFileTree(
             path,
-            object : java.nio.file.SimpleFileVisitor<java.nio.file.Path>() {
+            object : SimpleFileVisitor<Path>() {
                 override fun visitFile(
-                    file: java.nio.file.Path,
-                    attrs: java.nio.file.attribute.BasicFileAttributes?
-                ): java.nio.file.FileVisitResult {
+                    file: Path,
+                    attrs: BasicFileAttributes?
+                ): FileVisitResult {
                     // println("${file.absolutePathString()} to delete")
-                    java.nio.file.Files.delete(file)
+                    Files.delete(file)
                     return super.visitFile(file, attrs)
                 }
 
                 override fun postVisitDirectory(
-                    dir: java.nio.file.Path?,
+                    dir: Path?,
                     exc: java.io.IOException?
-                ): java.nio.file.FileVisitResult {
+                ): FileVisitResult {
                     if (dir != null) {
-                        java.nio.file.Files.delete(dir)
+                        Files.delete(dir)
                     }
                     return super.postVisitDirectory(dir, exc)
                 }
@@ -175,21 +180,21 @@ fun deleteDirectoryByNio(dir: String) {
 
 
 fun deleteExtraFiles(
-    destinationDirectory: java.nio.file.Path,
-    sourceDirectory: java.nio.file.Path,
+    destinationDirectory: Path,
+    sourceDirectory: Path,
     condition: ((fileName: String) -> Boolean) = ::noFilter
 ) {
     // delete file not in
-    java.nio.file.Files.walk(destinationDirectory)
+    Files.walk(destinationDirectory)
         .filter { path -> path != destinationDirectory }
         .filter { path ->
-            !java.nio.file.Files.exists(sourceDirectory.resolve(destinationDirectory.relativize(path)))
+            !Files.exists(sourceDirectory.resolve(destinationDirectory.relativize(path)))
                     || !condition.invoke(path.fileName.toString())
         }
         .forEach { path ->
             try {
-                if (!java.nio.file.Files.isDirectory(path)) { // Skip dir, avoid directory not empty exception
-                    java.nio.file.Files.delete(path)
+                if (!Files.isDirectory(path)) { // Skip dir, avoid directory not empty exception
+                    Files.delete(path)
                     println("Deleted extra file: ${destinationDirectory.relativize(path)}")
                 }
             } catch (e: java.io.IOException) {
@@ -198,7 +203,7 @@ fun deleteExtraFiles(
         }
 }
 
-fun noFilter(fileName: String) = true
+fun noFilter(@Suppress("UNUSED_PARAMETER") fileName: String) = true
 fun ownCondition(fileName: String): Boolean {
     /*TODO custom your own condition filter*/
     /*return fileName.endsWith(".api.kt")*/
@@ -213,9 +218,9 @@ fun syncDirectory(
     des: String,
     condition: ((fileName: String) -> Boolean) = ::noFilter
 ) {
-    val sourceDirectory: java.nio.file.Path = java.nio.file.Paths.get(src)
+    val sourceDirectory: Path = Paths.get(src)
 
-    if (!java.nio.file.Files.exists(sourceDirectory) || !java.nio.file.Files.isDirectory(
+    if (!Files.exists(sourceDirectory) || !Files.isDirectory(
             sourceDirectory
         )
     ) {
@@ -223,11 +228,11 @@ fun syncDirectory(
         return
     }
 
-    val destinationDirectory: java.nio.file.Path = java.nio.file.Paths.get(des)
+    val destinationDirectory: Path = Paths.get(des)
 
-    if (!java.nio.file.Files.exists(destinationDirectory)) {
+    if (!Files.exists(destinationDirectory)) {
         try {
-            java.nio.file.Files.createDirectories(destinationDirectory)
+            Files.createDirectories(destinationDirectory)
             println("Created destination directory: $destinationDirectory")
         } catch (e: java.io.IOException) {
             e.printStackTrace()
@@ -240,50 +245,50 @@ fun syncDirectory(
     }
 
     try {
-        java.nio.file.Files.walkFileTree(
+        Files.walkFileTree(
             sourceDirectory,
-            object : java.nio.file.SimpleFileVisitor<java.nio.file.Path>() {
+            object : SimpleFileVisitor<Path>() {
                 override fun visitFile(
-                    file: java.nio.file.Path,
-                    attrs: java.nio.file.attribute.BasicFileAttributes
-                ): java.nio.file.FileVisitResult {
+                    file: Path,
+                    attrs: BasicFileAttributes
+                ): FileVisitResult {
                     // Skip filter
                     if (!condition.invoke(file.fileName.toString())) {
-                        return java.nio.file.FileVisitResult.CONTINUE
+                        return FileVisitResult.CONTINUE
                     }
 
-                    val relativePath: java.nio.file.Path = sourceDirectory.relativize(file)
-                    val destinationFile: java.nio.file.Path =
+                    val relativePath: Path = sourceDirectory.relativize(file)
+                    val destinationFile: Path =
                         destinationDirectory.resolve(relativePath)
 
-                    java.nio.file.Files.copy(
+                    Files.copy(
                         file,
                         destinationFile,
-                        java.nio.file.StandardCopyOption.REPLACE_EXISTING
+                        StandardCopyOption.REPLACE_EXISTING
                     )
                     // println("Copied file: $relativePath")
-                    return java.nio.file.FileVisitResult.CONTINUE
+                    return FileVisitResult.CONTINUE
                 }
 
                 override fun preVisitDirectory(
-                    dir: java.nio.file.Path,
-                    attrs: java.nio.file.attribute.BasicFileAttributes
-                ): java.nio.file.FileVisitResult {
-                    val relativePath: java.nio.file.Path = sourceDirectory.relativize(dir)
-                    val destinationDir: java.nio.file.Path =
+                    dir: Path,
+                    attrs: BasicFileAttributes
+                ): FileVisitResult {
+                    val relativePath: Path = sourceDirectory.relativize(dir)
+                    val destinationDir: Path =
                         destinationDirectory.resolve(relativePath)
 
-                    if (!java.nio.file.Files.exists(destinationDir)) {
+                    if (!Files.exists(destinationDir)) {
                         try {
-                            java.nio.file.Files.createDirectories(destinationDir)
+                            Files.createDirectories(destinationDir)
                             // println("Created directory: $relativePath")
                         } catch (e: java.io.IOException) {
                             e.printStackTrace()
-                            return java.nio.file.FileVisitResult.TERMINATE
+                            return FileVisitResult.TERMINATE
                         }
                     }
 
-                    return java.nio.file.FileVisitResult.CONTINUE
+                    return FileVisitResult.CONTINUE
                 }
             })
 
@@ -297,7 +302,19 @@ fun syncDirectory(
  * Create build.gradle.kts
  * @param isJava Android module or Java module
  * */
-fun createBuildGradle(scriptPath: String, selfName: String, isJava: Boolean = false) {
+fun generateBuildGradle(
+    scriptDir: String,
+    scriptName: String,
+    selfName: String,
+    isJava: Boolean = false
+) {
+    // Ensure _api directory is created!
+    java.io.File(scriptDir).let {
+        if (!it.exists()) {
+            it.mkdir()
+        }
+    }
+    val scriptPath = "${scriptDir}${java.io.File.separator}${scriptName}"
     val buildScript = java.io.File(scriptPath)
     val path = if (isJava) {
         BUILD_TEMPLATE_PATH_JAVA
@@ -310,7 +327,7 @@ fun createBuildGradle(scriptPath: String, selfName: String, isJava: Boolean = fa
         throw Exception("Template file ${templateFile.absolutePath} not found!")
     }
     val readText = templateFile.readText()
-    val copyText = String.format(readText, "cn.jailedbird.module.${selfName}_api")
+    val copyText = String.format(readText, String.format(MODULE_NAMESPACE_TEMPLATE, selfName))
     buildScript.writeText(copyText)
 }
 
@@ -322,9 +339,9 @@ fun deleteEmptyDir(path: String) {
 // A包含A1 A2 A3 AN AN全为空 A不会被删除
 fun deleteEmptyDir(file: java.io.File) {
     if (file.isDirectory) {
-        file.listFiles().forEach {
+        file.listFiles()?.forEach {
             if (it.isDirectory) {
-                if (it.listFiles().isEmpty()) {
+                if (it.listFiles()?.isEmpty() == true) {
                     it.delete() // 删除后 file对象依然存在
                     if (it.parentFile?.listFiles()?.isEmpty() == true) {
                         it.parentFile.delete() // 使用file对象尝试删除父目录
@@ -341,7 +358,7 @@ fun deleteEmptyDir(file: java.io.File) {
  * @param src0 : such as module1 abs path
  * */
 @Deprecated("Deprecated because of low performance")
-fun doSyncByGraldeApi(src0: String) {
+fun doSyncByGradleApi(src0: String) {
     val t1 = System.currentTimeMillis()
     val src = "${src0}${java.io.File.separator}src${java.io.File.separator}main"
     val des = "${src0}_api${java.io.File.separator}src${java.io.File.separator}main"
