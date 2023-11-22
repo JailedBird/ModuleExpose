92be5705501ecbf4e3584dff4dc321957d2aa482
JailedBird <zjc986812982@gmail.com>
2023-11-22 19:46:41 +0800

    feat: remove android_java gradle

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -53,10 +53,6 @@ gradlePlugin {
             id = "nowinandroid.android.application"
             implementationClass = "AndroidApplicationConventionPlugin"
         }
-        register("androidJava") {
-            id = "nowinandroid.android.java"
-            implementationClass = "AndroidJavaConventionPlugin"
-        }
         register("androidApplicationJacoco") {
             id = "nowinandroid.android.application.jacoco"
             implementationClass = "AndroidApplicationJacocoConventionPlugin"
