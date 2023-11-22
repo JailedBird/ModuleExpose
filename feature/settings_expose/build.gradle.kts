311512a86f718d16b74806cf849ff0d73e9107d7
JailedBird <zjc986812982@gmail.com>
2023-11-22 15:35:22 +0800

    update .gitignore

--- feature/settings_expose/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.nowinandroid.android.library)
-}
-
-android {
-    namespace = "cn.jailedbird.module.settings_expose"
-}
--- feature/settings_expose/src/main/java/cn/jailedbird/feature/settings/expose/SettingExpose.kt
+++
@@ -1,7 +0,0 @@
-package cn.jailedbird.feature.settings.expose
-
-import android.content.Context
-
-interface SettingExpose {
-    fun startSettingActivity(context: Context)
-}
